2cd4d64b3f4e19712618f160388d01dc626f07e8
upgle <admin@upgle.com>
2015-11-20 03:47:57 +0900

    bench mark 추가 및 디자인 추가 작업.

--- example/index.php
+++ example/index.php
@@ -5,23 +5,50 @@ use Upgle\Importer\ExcelImporter;
 use Upgle\Algorithm\Dijkstra;
 use Upgle\Model\SeoulMetro;
 
+/**
+ * Composer Autoload
+ */
 require_once(dirname(__FILE__). '/../vendor/autoload.php');
 define('EXCEL_PATH', dirname(__FILE__). '/metro.xlsx');
 date_default_timezone_set('Europe/London');
 
+/**
+ * Initilize SeoulMetro Graph
+ */
 $seoulMetro = new SeoulMetro();
+
+/**
+ * Import Data From Excel
+ */
 $importer = new ExcelImporter(EXCEL_PATH, $seoulMetro);
 $importer->import();
 
+/**
+ * Benchmark Dijkstra Algorithm
+ */
+$bench = new \Ubench();
+$bench->start();
 $algorithm = new Dijkstra($seoulMetro);
-$path = $algorithm->getShortestPath(
-    $seoulMetro->getStationIdByName("망월사", "1"),
-    $seoulMetro->getStationIdByName("시청", "1")
-);
+$path = [];
+if(is_numeric($_GET["start"]) && is_numeric($_GET["goal"])) {
+    $path = $algorithm->getShortestPath(
+        $_GET["start"],
+        $_GET["goal"]
+    );
+}
+$bench->end();
 
+/**
+ * get Stations list
+ */
 $stations = [];
 foreach($seoulMetro->getVertices() as $station) {
-    $stations[$station->getId()] = $station->getName();
+    /* @var \Upgle\Model\Station $station */
+    $stations[] = [
+        "id" => $station->getId(),
+        "name" => $station->getName(),
+        "line" => $station->getLine()
+    ];
 }
 ?>
 <!DOCTYPE html>
@@ -30,6 +57,8 @@ foreach($seoulMetro->getVertices() as $station) {
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
     <title>Simple Polylines</title>
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <script src="js/typeahead.bundle.min.js"></script>
     <style>
         html, body {
             height: 100%;
@@ -46,8 +75,20 @@ foreach($seoulMetro->getVertices() as $station) {
             z-index: 100;
             background: #fff;
         }
+        #nav .statistics {
+            float: right;
+            padding: 0 5px 0 0;
+            margin: 0;
+            font-size: 12px;
+            line-height: 50px;
+        }
+        #nav .statistics li {
+            display: inline-block;
+            list-style: none;
+            padding-right: 15px;
+        }
         #map {
-            margin-left:275px;
+            margin-left:280px;
             height: 100%;
         }
         #sidebar {
@@ -55,16 +96,20 @@ foreach($seoulMetro->getVertices() as $station) {
             position: absolute;
             left: 0;
             top: 0;
-            width: 275px;
+            width: 280px;
             background: #fff;
             border-right: 1px solid #aaa;
             z-index: 90;
             font-size: 13px;
+            padding-top: 165px;
             box-sizing: border-box;
         }
         #subway-route {
             margin: 0;
-            padding: 60px 0 0;
+            padding: 10px 0 0;
+            height: 100%;
+            overflow: auto;
+            box-sizing: border-box;
         }
         #subway-route li {
             position: relative;
@@ -108,12 +153,106 @@ foreach($seoulMetro->getVertices() as $station) {
             height: 19px;
             content : "";
         }
+
+        #form-search {
+            position: absolute;
+            top: 49px;
+            background: #fafafa;
+            padding: 16px 17px;
+            border-bottom: 1px solid #eee;
+        }
+        #form-search input[type=text] {
+            border: 1px solid #ddd;
+            width: 100%;
+            height: 31px;
+            margin-bottom: 5px;
+            padding: 0 10px;
+            box-sizing: border-box;
+        }
+        #form-search .btn-search {
+            height: 35px;
+            margin-top: 5px;
+            background: #ffffff;
+            border: 1px solid #bbb;
+            cursor: pointer;
+            width: 100%;
+            font-size: 12px;
+        }
+
+        .twitter-typeahead {
+            width: 100%;
+        }
+        .typeahead {
+            background-color: #fff;
+        }
+
+        .typeahead:focus {
+            border: 2px solid #0097cf;
+        }
+
+        .tt-query {
+            -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+            -moz-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+            box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+        }
+
+        .tt-hint {
+            color: #999
+        }
+
+        .tt-menu {
+            width: 245px;
+            margin: 12px 0;
+            padding: 8px 0;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            -webkit-border-radius: 2px;
+            -moz-border-radius: 2px;
+            border-radius: 2px;
+            -webkit-box-shadow: 0 5px 5px rgba(0,0,0,.1);
+            -moz-box-shadow: 0 5px 5px rgba(0,0,0,.1);
+            box-shadow: 0 5px 5px rgba(0,0,0,.1);
+        }
+        .tt-suggestion {
+            padding: 3px 20px;
+            font-size: 14px;
+            line-height: 24px;
+        }
+        .tt-suggestion:hover {
+            cursor: pointer;
+            color: #fff;
+            background-color: #0097cf;
+        }
+        .tt-suggestion.tt-cursor {
+            color: #fff;
+            background-color: #0097cf;
+
+        }
+        .tt-suggestion p {
+            margin: 0;
+        }
+
+
     </style>
 </head>
 <body>
 <div id="nav">
+    <ul class="statistics">
+        <li>알고리즘 연산 시간 : <strong><?=$bench->getTime()?></strong></li>
+        <li>메모리 피크 : <strong><?=$bench->getMemoryPeak()?></strong></li>
+    </ul>
 </div>
 <div id="sidebar">
+
+    <form action="/" id="form-search">
+    <input name="goal" class="goal" type="hidden" />
+    <input name="start" class="start" type="hidden" />
+
+    <input class="start_typeahead" type="text" placeholder="출발 역">
+    <input class="goal_typeahead" type="text" placeholder="도착 역">
+    <input class="btn-search" type="submit" value="빠른길 찾기" />
+    </form>
+
     <ul id="subway-route">
         <?php foreach ($path as $station) : ?>
         <li class="line<?=$station->getLine()?>"><span class="mark"></span><?=$station->getName()?></li>
@@ -141,6 +280,27 @@ foreach($seoulMetro->getVertices() as $station) {
 
         flightPath.setMap(map);
     }
+
+    var states = <?=json_encode($stations)?>;
+    states = new Bloodhound({
+        datumTokenizer: function(d) {
+            return Bloodhound.tokenizers.whitespace(d.name);
+        },
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        identify: function(obj) { return obj.id; },
+        // `states` is an array of state names defined in "The Basics"
+        local: states
+    });
+    $('.start_typeahead').typeahead({ hint: true, highlight: true, minLength: 1 },
+        { name: 'states', displayKey: "name", source: states
+        }).on('typeahead:selected', function(event, data){
+            $('.start').val(data.id);
+        });
+    $('.goal_typeahead').typeahead({ hint: true, highlight: true, minLength: 1 },
+        { name: 'states', displayKey: "name", source: states
+        }).on('typeahead:selected', function(event, data){
+            $('.goal').val(data.id);
+        });
 </script>
 <script async defer
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBeu1Js2eiRwejCHm4gmhaE8I4Oxg-BFSg&signed_in=true&callback=initMap"></script>
