7caadfb70fd1bca3fb0a51d9828141b66f49203b
upgle <admin@upgle.com>
2015-11-22 00:22:12 +0900

    Vertex Id로 역 이름 반환하는 메소드 추가.

--- src/Model/SeoulMetro.php
+++ src/Model/SeoulMetro.php
@@ -6,7 +6,7 @@ class SeoulMetro extends Graph
     private $alias = [];
 
     /**
-     * @param Station $vertex
+     * @param Vertex $vertex
      */
     public function setVertex(Vertex $vertex) {
         $this->vertices[$vertex->getId()] = $vertex;
@@ -21,6 +21,17 @@ class SeoulMetro extends Graph
     public function getStationIdByName($stationName, $stationLine) {
         if(isset($this->alias[$stationLine][$stationName])) {
             return $this->alias[$stationLine][$stationName];
+        }
+        return NULL;
+    }
+
+    /**
+     * @param $stationId
+     * @return null
+     */
+    public function getStationNameById($stationId) {
+        if($this->getVertexById($stationId) !== NULL) {
+            return $this->getVertexById($stationId)->getName();
         }
         return NULL;
     }
